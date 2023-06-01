82385271f8e33e8f6469a755767bfd8241b229e9
Parfait Vertie <parfaitv@oa-j3.msk.21-school.ru>
2023-06-01 13:32:27 +0300

    :sparkles: release :pencil: docs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const verseChoose = document.querySelector('select');
 const apiKey = '87dd2123224c395804051611e36c7b00';
-// const apiKeyMapGoogle = 'AIzaSyDh1b4v4aX36DLX9plP5wP5_2TT6F637TQ'
+const verseChoose = document.querySelector('select');
 const resultCity = document.querySelector('.city');
 const resultfromTo = document.querySelector('.fromTo');
 const descrip = document.querySelector('.descrip');
@@ -14,22 +13,20 @@ let feelsLike = document.createElement('p');
 
 verseChoose.addEventListener('change', () => {
     const verse = verseChoose.value;
-    displayShow(verse);
+    showWeather(verse);
 });
 
-async function displayShow(city) {
+async function showWeather(city) {
     try{
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
         let response = await fetch(url);
         let res = await response.json();  
                                             // Get the current time in the city. Working version. 
-        // let urlDateTime = `http://htmlweb.ru/json/geo/timezone?latitude=${res.data.coord.lat}&longitude=${res.data.coord.lon}&country=RU`;
+        // let urlDateTime = `http://htmlweb.ru/json/geo/timezone?latitude=${res.coord.lat}&longitude=${res.coord.lon}&country=RU`;
         // axios.get(urlDateTime).then(dateTime => {
         //     let nowDate = `${new Date().getDate()}.${new Date().getMonth().toString().padStart(2,'0')}.${new Date().getFullYear()}`
         //     todayDate.innerHTML = `${nowDate} ${dateTime.data.time}`;
-        //     console.log(dateTime)
         // }); 
-        // console.log(res);
         IconImg.src = `https://openweathermap.org/img/wn/${res.weather[0]['icon']}@2x.png`;
         resultCity.innerHTML = res.name;
         resultfromTo.innerHTML = ` ${Math.round(res.main.temp_min)} / ${Math.round(res.main.temp_max)}°C`;
@@ -43,5 +40,5 @@ async function displayShow(city) {
         console.log(error);
     }
 };
-verseChoose.value = 'Moscow';
-displayShow('Moscow');
+verseChoose.value = 'Москва';
+showWeather('Москва');
